9b8b12ffdd58e510d4e47cd23b38e55d577bc744
Joel McDonald <joel@mcdwebworks.com>
2021-02-22 12:44:44 -0500

    Update get_cdn_boostrap.php
    Updated to Bootstrap 4.6.

--- get_cdn_boostrap.php
+++ get_cdn_boostrap.php
@@ -9,11 +9,12 @@ function get_cdn_boostrap() {
 
 	// Add latest version of Boostrap 4.
 
-	wp_enqueue_style('bootstrap-cdn', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css');
+	wp_enqueue_style('bootstrap-cdn', 'https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css');
 
-	wp_enqueue_script( 'popper-cdn','https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js', array( 'jquery' ),'',true );
+	wp_enqueue_script( 'popper-cdn','https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js', array( 'jquery' ),'',true );
 
-	wp_enqueue_script( 'bootstrap-cdn','https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js', array( 'jquery' ),'',true );
+	wp_enqueue_script( 'bootstrap-cdn','https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js', array( 'jquery' ),'',true );
+	
 }
 
 add_action( 'wp_enqueue_scripts', 'get_cdn_boostrap' );
